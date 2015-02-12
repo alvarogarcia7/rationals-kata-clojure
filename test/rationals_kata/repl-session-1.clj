d4628d7190b4c603531d50e168939849bf9727e6
alvaro garcia <derecerca@gmail.com>
2015-02-12 03:59:21 +0100

    add repl-session with cases in comments

---
+++ test/rationals_kata/repl-session-1.clj
@@ -0,0 +1,13 @@
+; cases - scratchpad
+
+; a rational = n / m - numerator / denominator
+; examples:
+;   0 + 0 = 0
+;   0 + 1 = 1
+;   1 + 1 = 2
+;   1 + 1/2 = 3/2
+;   1/2 + 1 = 3/2
+;   1/2 + 1/2 = 1
+;   1/6 + 1/6 = 1/3
+;   1/2 + 1/8 = 5/8
+;   1/2 + 2/8 = 3/4
