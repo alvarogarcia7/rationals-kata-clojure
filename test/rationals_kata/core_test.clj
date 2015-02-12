d7f5b6a98db5f92af305d96964542e14816333b5
alvaro garcia <derecerca@gmail.com>
2015-02-12 03:51:03 +0100

    add all contents from default project structure

---
+++ test/rationals_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns rationals-kata.core-test
+  (:require [clojure.test :refer :all]
+            [rationals-kata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
