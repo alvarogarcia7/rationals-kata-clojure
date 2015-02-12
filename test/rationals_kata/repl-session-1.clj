37fca6218a4a4c41831ecd51712be2c78ef627af
alvaro garcia <derecerca@gmail.com>
2015-02-12 04:11:26 +0100

    sort cases by difficulty

--- test/rationals_kata/repl-session-1.clj
+++ test/rationals_kata/repl-session-1.clj
@@ -6,10 +6,11 @@
 ;   0 + 1 = 1 ; a zero with a non-zero
 ;   1 + 1 = 2 ; two non-zeros
 ;   1 + 1/2 = 3/2 ; different denominators, no need to simplify
-;   1/2 + 1/2 = 1 ; same denominator, need to simplify
-;   1/2 + 1/8 = 5/8 ; denominators are one multiple of the other, no need to simplify
-;   1/2 + 2/8 = 3/4 ; denominators are one multiple of the other, need to simplify
 ;   8/5 + 5/8 = 89/40 ; inverse fractions, numbers are coprime between each other (share no common prime) - because fractions were already simplified
+
+;   1/2 + 1/8 = 5/8 ; denominators are one multiple of the other, no need to simplify
+;   1/2 + 1/2 = 1 ; same denominator, need to simplify
+;   1/2 + 2/8 = 3/4 ; denominators are one multiple of the other, need to simplify
 ;   6/14 + 14/6 = 58/21 ; inverse fractions, fractionals are not simplified (they are not coprime)
 
 (+ 15/16 16/15)
